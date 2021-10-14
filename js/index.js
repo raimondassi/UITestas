9637d7eeecf676abb25ad48dccf0cd2a51c0231b
raimondassi <raimondassiup@gmail.com>
2021-10-14 21:02:49 +0300

    paskutinis idejimas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
-$(function() {
+$(function(){
+    var name = $('#name');
+    var price = $('#price');
+    var quantity = $('#quantity');
 
-    console.log('naujas');
+    var products = JSON.parse(localStorage.getItem('products'));
+    if (products === null) {
+        products = [];
+    }
 
+    $('#enterButton').click(function(event) {
+        event.preventDefault();
+        let procucts = createProductObject(name.val(), price.val(), quantity.val());
+        saveProduct(procucts);
+        cleanUp();
+    });
+ 
+    function saveProduct(product) {
+        products.push(product);
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+
+    function createProductObject(name, price, quantity) {
+        let product = {
+            name: name,
+            price: price,
+            quantity: quantity
+        }
+
+        return product;
+    }
+
+    function cleanUp(){
+        name.val('');
+        price.val('');
+        quantity.val('');
+    }
+    
 });
